d4d0bb2b851ddfa6e3fc163590f8f8e6cdc145b3
sphinxxxx <coding@enthuses.me>
2018-08-21 00:46:18 +0000

    NPM, plus transpiling issue

--- rollup.config.js
+++ rollup.config.js
@@ -41,6 +41,15 @@ export default {
         }),
         
         babel({
+            //Looks like this is used as the default config if no .babelrc file is present,
+            //and we need this, or else dragTracker won't be transpiled (it doesn't have a .babelrc file).
+            //Thus, we can probably delete the .babelrc in /src, because the config is the same.
+            //(.babelrc files are even discouraged: https://babeljs.io/docs/en/next/babelrc)
+            presets: [
+                ["env", { modules: false }],
+            ],
+            plugins: ["external-helpers"],
+
             //Must transpile dragTracker module..
             //  exclude: 'node_modules/**', // only transpile our source code
         }),
